9609295569638c482f93aa84629c887f18cb05ab
Dave Bush <github@david-bush.co.uk>
2017-01-03 15:29:53 +1300

    Added tests for message list.

--- logs.py
+++ logs.py
@@ -9,8 +9,16 @@ class LogHandler(object):
     def __init__(self, location=None):
         self.location = location if location else os.environ.get(
             'ZNC_LOG_LOCATION', '.')
-        logger.info('LogHandler location set to {}'.format(self.location))
+        # logger.info('LogHandler location set to {}'.format(self.location))
 
     def get_logs_list(self):
         return sorted(
             [f for f in os.listdir(self.location) if f.endswith('.log')])
+
+    def get_message_list(self):
+        message_list = []
+        for f in self.get_logs_list():
+            with open(self.location + '/' + f) as log_file:
+                for line in log_file:
+                    message_list.append(line.strip())
+        return message_list
--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -5,6 +5,14 @@ from twisted.trial import unittest
 
 from logs import LogHandler
 
+TEST_LOG_FILE_METADATA = {
+    'total_length': 76,
+    'first_head': '[00:48:30] <asdf> _habnabit yes heartache is amazing',
+    'second_head': '[04:10:55] <toddpratt> but the only way to diagnose '
+    'it positively is via autopsy',
+    'third_head': '[05:36:39] <Jerub> is the daydream vr thing any good?'
+}
+
 
 class LogHandlingTests(unittest.TestCase):
     def setUp(self):
@@ -34,7 +42,24 @@ class LogHandlingTests(unittest.TestCase):
             self.assertTrue(f.endswith('log'))
 
     def test_log_file_order(self):
-        self.assertItemsEqual(self.log_handler.get_logs_list(), sorted(self.log_files))
+        self.assertItemsEqual(self.log_handler.get_logs_list(), sorted(
+            self.log_files))
 
+    def test_get_message_list(self):
+        message_list = self.log_handler.get_message_list()
+        self.assertEqual(
+            len(message_list), TEST_LOG_FILE_METADATA['total_length'])
 
+    def test_message_does_not_end_with_new_line(self):
+        message_list = self.log_handler.get_message_list()
+        for m in message_list:
+            self.assertFalse(m.endswith('\\n'))
 
+    def test_get_message_list_order(self):
+        message_list = self.log_handler.get_message_list()
+        self.assertEqual(
+            message_list[0], TEST_LOG_FILE_METADATA['first_head'])
+        self.assertEqual(
+            message_list[25], TEST_LOG_FILE_METADATA['second_head'])
+        self.assertEqual(
+            message_list[51], TEST_LOG_FILE_METADATA['third_head'])
