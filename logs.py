2645587f07091c351209b417bdba2f5b4e4d1129
Dave Bush <github@david-bush.co.uk>
2017-01-03 14:54:16 +1300

    Added logging.

--- logs.py
+++ logs.py
@@ -1,10 +1,15 @@
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class LogHandler(object):
     def __init__(self, location=None):
         self.location = location if location else os.environ.get(
             'ZNC_LOG_LOCATION', '.')
+        logger.info('LogHandler location set to {}'.format(self.location))
 
     def get_logs_list(self):
         return sorted(
